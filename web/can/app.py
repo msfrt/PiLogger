551e154a74ca053efd3074130462fab370c42dcd
Gabriel Sotelo <gabrielsotelojusto@hotmail.com>
2021-01-06 17:11:06 -0500

    can

--- web/can/app.py
+++ web/can/app.py
@@ -25,7 +25,7 @@ def success():
 def send_message(ID, data):
     bus = can.interface.Bus(bustype=bustype, channel=channel)
     msg = can.Message(arbitration_id=ID,
-                      data= [x for x in str(data)],
+                      data= [x for x in data],
                       is_extended_id=True)
     bus.send(msg)
     
