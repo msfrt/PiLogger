ef5b29c9c1ea92287f84e66b23e66d12322213c7
Dave Yonkers <yonkers4@msu.edu>
2021-01-07 19:26:59 -0500

    Successfully add yaml-cpp and added basic CLI input options

---
+++ logger/src/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+#include "yaml-cpp/yaml.h"
+
+using namespace::std;
+
+/// The default name of the logger configuration file
+const string DEFAULT_CONFIG_FILE = "logger.conf";
+
+
+int main(int argc, char *argv[])
+{
+
+    string config_file = DEFAULT_CONFIG_FILE;
+    for (int i = 1; i < argc; i++){
+
+        if (string(argv[i]) == "-f" && i < argc - 1) {
+            config_file = argv[i + 1];
+
+        } else if (string(argv[i]) == "-f") {
+            cerr << "ERROR: No config filename was included!" << endl;
+            exit ( 1 );
+        }
+
+    }
+
+    // load config file into a yaml node
+    YAML::Node config;
+    try {
+        config = YAML::LoadFile(config_file);
+    } catch (YAML::BadFile) {
+        cerr << "ERROR: Bad configuration file: " << config_file << endl;
+        exit ( 1 );
+    }
+
+    if (config["busses"]) {
+        for (YAML::const_iterator bus = config["busses"].begin(); bus != config["busses"].end(); ++bus) {
+            std::cout << bus->as<string>() << "\n";
+        }
+    }
+
+    return 0;
+}
