9abdb71d4759c327e82c4320c9641292aa8e4778
Dave Yonkers <yonkers4@msu.edu>
2021-01-15 19:03:22 -0500

    Successfully read CAN messages off a threaded monitor and virtual interface

--- logger/src/main.cpp
+++ logger/src/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <string>
+#include <vector>
 #include <pthread.h>
+#include <signal.h>
 #include "yaml-cpp/yaml.h"
 #include "ThreadableMsgQueue.hpp"
 #include "monitor.hpp"
@@ -12,14 +14,76 @@ const string DEFAULT_CONFIG_FILE = "logger.conf";
 
 /// setting this boolean as true will output debugging statements to
 /// the standard output stream
-const bool LOGGER_DEBUG = true;
+extern const bool LOGGER_DEBUG = true;
+
+/// This number probably shouldn't be messed with. It's just so that we don't have
+/// to deal with dynamic array allocation and whatnot
+const int MAX_THREADS = 100;
 
 /// Global queue to be accessed by socket monitoring threads and
 /// decoder thread
-CThreadableMsgQueue decoderQueue;
+CThreadableMsgQueue decoder_queue;
+
+/// global vector to store the pthread objects for monitoring threads
+pthread_t monitoring_threads[MAX_THREADS];
+
+/// A count for the number of monitoring threads contained in the monitoring
+/// thread array
+int monitor_count = 0;
+
+/// global for threads to monitor and see if they need to exit
+extern bool stop_logging = false;
+
+
+/**
+ * A simple function that joins threads when given an array of thread IDs
+ * \param threads The vector of thread IDs
+ */
+int join_threads(pthread_t threads[], int count){
+    for (long n = 0; n < count; n++) {
+        if (pthread_join(threads[n], NULL)) {
+            cerr << "ERROR: Failed to join thread #" <<  n << "!" << endl;
+            return 2;
+        } 
+    }
+    return 0;
+}
+
+
+/**
+ * Signal callback handler to catch runtime signals such as CTRL-C to quit.
+ * https://www.tutorialspoint.com/how-do-i-catch-a-ctrlplusc-event-in-cplusplus
+ * \param signum The number that corresponds to the signal (2 is CTRL-C)
+ */
+void signal_callback_handler(int signum) {
+   if (LOGGER_DEBUG) cout << "Caught signal " << signum << endl;
+
+   switch (signum)
+   {
+   case 2:
+        stop_logging = true;
+        join_threads(monitoring_threads, monitor_count);
+
+        // Terminate program
+        exit(signum);
+   
+   default:
+       break;
+   }
+   
+}
+
+
+
+
 
 int main(int argc, char *argv[])
 {
+
+    // register the handler function for user signals (such as CTRL-C)
+    // https://www.tutorialspoint.com/how-do-i-catch-a-ctrlplusc-event-in-cplusplus
+    signal(SIGINT, signal_callback_handler);
+
 
     string config_file = DEFAULT_CONFIG_FILE;
     for (int i = 1; i < argc; i++){
@@ -45,13 +109,11 @@ int main(int argc, char *argv[])
 
 
     // loop through the specified interfaces to grab the number of required monitoring threads
-    int socketCount = 0;
     if (config["interfaces"]) {
 
         for (YAML::const_iterator iface = config["interfaces"].begin(); iface != config["interfaces"].end(); ++iface) {
-
+            monitor_count++;
             if (LOGGER_DEBUG) std::cout << "Found " << iface->as<string>() << " in config." << endl;
-            socketCount++;
         }
 
     } else {
@@ -61,17 +123,16 @@ int main(int argc, char *argv[])
 
 
     // loop through the configured CAN interfaces and create threads for their monotors
-    pthread_t monitorThreads[socketCount];
-    MonitorParams paramsArray[socketCount];
-    int currentThreadIndex = 0;
+    MonitorParams params_ary[monitor_count];
+    int current_thread_idx = 0;
     for (YAML::const_iterator iface = config["interfaces"].begin(); iface != config["interfaces"].end(); ++iface) {
 
         // populate the parameters for the current interface's monitoring thread
-        paramsArray[currentThreadIndex].iface_name = iface->as<string>();
-        paramsArray[currentThreadIndex].queue      = &decoderQueue;
+        params_ary[current_thread_idx].iface_name = iface->as<string>();
+        params_ary[current_thread_idx].queue      = &decoder_queue;
         
         // create the thread
-        if (pthread_create(&monitorThreads[currentThreadIndex], NULL, monitor, (void *)(&paramsArray[currentThreadIndex]))) {
+        if (pthread_create(&monitoring_threads[current_thread_idx], NULL, monitor, (void *)(&params_ary[current_thread_idx]))) {
             cerr << "ERROR: Failed to create " <<  iface->as<string>() << " monitoring thread!" << endl;
             exit( 1 );
         }
@@ -79,14 +140,19 @@ int main(int argc, char *argv[])
     }
 
 
-    // join the producer threads
-    for (long n = 0; n < socketCount; n++) {
-        if (pthread_join(monitorThreads[n], NULL)) {
-            cerr << "ERROR: Failed to join thread #" <<  n << "!" << endl;
-            exit( 2 );
-        } 
-    }
+
+
+
+
+
+
+    // at this point, we're done, join the producer threads after they've exited here
+    join_threads(monitoring_threads, monitor_count);
 
 
     return 0;
 }
+
+
+
+
--- logger/src/monitor.cpp
+++ logger/src/monitor.cpp
@@ -4,6 +4,38 @@
  * \author Dave Yonkers
  */
 
+#include <iostream>
+#include <string.h>
+
+#include <unistd.h>
+#include <net/if.h>
+#include <sys/ioctl.h>
+#include <sys/socket.h>
+
+#include <linux/can.h>
+#include <linux/can/raw.h>
+
+#include "monitor.hpp"
+
+using namespace std;
+
+extern const bool LOGGER_DEBUG;
+extern bool stop_logging;
+
+
+/**
+ * A simple function to gracefully close a socket. I made this a function so that
+ * it'd be easy to close a socket in more than one place.
+ * \param socket_num The index of the socket to close
+ */
+void CloseSocket(int socket_num){
+    if (close(socket_num) < 0) {
+		cerr << "ERROR: Problem closing socket ID " << socket_num << endl;
+        return;
+	}
+    if (LOGGER_DEBUG) cout << "Successfully closed socket ID " << socket_num << endl;
+}
+
 
 /**
  * CAN socket monitoring thread main function.
@@ -12,13 +44,74 @@
  * thrown into a CMessage, which records the time and the interface name. The
  * CMessage is then thrown into the decoding queue for further processing.
  */
-void* monitor(void*){
+void* monitor(void* args){
 
-    for (long i; i < 100000000; i++){
-        
+    // cast params pointer to a useable form (oooof, Dr. Owen would hate me)
+    MonitorParams params = *(MonitorParams*) args;
+
+
+    // open a socket
+    int s;
+    s = socket(PF_CAN, SOCK_RAW, CAN_RAW);
+    if (LOGGER_DEBUG)
+        cout << "Socket created for " << params.iface_name << " with return value: " << s << endl;
+    if (s < 0) {
+        cerr << "ERROR: could not open socket for " << params.iface_name << "!" << endl;
+        return (void*)1;
     }
+
+
+    // interface name and socket union
+    struct ifreq ifr;
+    strncpy(ifr.ifr_name, params.iface_name.c_str(), params.iface_name.length() + 1);
+	if (ioctl(s, SIOCGIFINDEX, &ifr) < 0){
+        cerr << "ERROR: could not join " << params.iface_name << " to socket ID " << 
+            s << "! Result: " << ifr.ifr_name << endl;
+        CloseSocket(s);
+        return (void*)1;
+    }
+
+
+    // bind the socket to the CAN interface index
+    struct sockaddr_can addr;
+
+    memset(&addr, 0, sizeof(addr));
+    addr.can_family = AF_CAN;
+    addr.can_ifindex = ifr.ifr_ifindex;
+
+    if (bind(s, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
+        cerr << "ERROR: could not bind socket " << s << " to interface " << 
+            params.iface_name << " (ifindex " << ifr.ifr_ifindex << ")" << endl;
+        CloseSocket(s);
+        return (void*)1;
+    }
+
+
+    // loop forever (or until asked to stop)
+    while (!stop_logging){
+
+        int nbytes;
+	    struct can_frame frame;
+
+        nbytes = read(s, &frame, sizeof(struct can_frame));
+
+        printf("0x%03X [%d] ",frame.can_id, frame.can_dlc);
+
+        for (int i = 0; i < frame.can_dlc; i++)
+            printf("%02X ",frame.data[i]);
+
+
+    }
+
+
+    // close the socket
+    CloseSocket(s);
 
     return nullptr;
 }
+
+
+
+
 
 
