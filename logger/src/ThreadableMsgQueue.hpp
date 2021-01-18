5ac84679b431d31a3fda000323c3f139d641eff7
Dave Yonkers <yonkers4@msu.edu>
2021-01-17 21:13:22 -0500

    Moved all queue implementation to the header file

--- logger/src/ThreadableMsgQueue.hpp
+++ logger/src/ThreadableMsgQueue.hpp
@@ -3,7 +3,8 @@
  *
  * \author Dave Yonkers
  *
- * Class that implements a circular queue that can be safely shared between threads
+ * Class that implements a circular queue that can be safely shared between threads.
+ * This is contained all in one file because it is templated.
  */
 
 #pragma once
@@ -22,7 +23,7 @@ class CReceivedMessage;
  * queue array (when attempting to insert), or if there are no messages to consume
  * (when requesting to pop a queue that's empty)
  */
-template <class queue_item_type>
+template <class queue_type>
 class CThreadableMsgQueue {
 
 public:
@@ -38,8 +39,14 @@ public:
     void SetBufferSize(int size);
     int Initialize();
 
-    void Push(queue_item_type item);
-    queue_item_type Pop();
+    /**
+     * Getter for the buffer size
+     * \returns The buffer size in slots
+     */ 
+    int GetBufferSize() const { return mItemsK; }
+
+    void Push(queue_type item);
+    queue_type Pop();
 
 private:
 
@@ -49,7 +56,7 @@ private:
     sem_t mE;  /// for the number of empty slots
 
     /// buffer that will hold the queue items
-    std::vector<queue_item_type> mBuffer;
+    std::vector<queue_type> mBuffer;
 
     int mItemsK   = 5000;  /// default buffer size
     int mItemsIn  = 0;     /// buffer index of the next insertable location
@@ -60,3 +67,89 @@ private:
 };
 
 
+/**
+ * Set the size of the queue before initializing the semaphores
+ * \param size The size of the queue buffer
+ */
+template <class queue_type>
+void CThreadableMsgQueue<queue_type>::SetBufferSize(int size) {
+    if (!mInitialized)
+        mItemsK = size;
+}
+
+
+/**
+ * Initialize the semaphores and the queue buffer if they
+ * haven't already been.
+ * \returns 0 on success, -1 on failure (program should exit)
+ */
+template <class queue_type>
+int CThreadableMsgQueue<queue_type>::Initialize() {
+    if (!mInitialized){
+
+        // initialize semaphores
+        if (sem_init(&mS, 0, 1) == -1)
+            return -1;
+
+        if (sem_init(&mN, 0, 0) == -1) 
+            return -1;
+
+        if (sem_init(&mE, 0, mItemsK) == -1) 
+            return -1;
+
+        // resize buffer
+        mBuffer.resize(mItemsK);
+
+        mInitialized = true;
+    }
+
+    return 0;
+}
+
+
+/**
+ * Push an item into the queue... safely
+ * \param item The item to push onto the top of the queue
+ */
+template <class queue_type>
+void CThreadableMsgQueue<queue_type>::Push(queue_type item) {
+
+    if (!mInitialized)
+        Initialize();
+
+    // insert the transaction into the buffer
+    sem_wait(&mE);
+    sem_wait(&mS);
+    mBuffer.at(mItemsIn) = item;
+    mItemsIn = (mItemsIn + 1) % mItemsK;
+    sem_post(&mS);
+    sem_post(&mN);
+
+}
+
+
+
+/**
+ * Pop an item from the queue... safely
+ * \returns The item that was popped from the bottom of the queue
+ */
+template <class queue_type>
+queue_type CThreadableMsgQueue<queue_type>::Pop() {
+
+    if (!mInitialized)
+        Initialize();
+
+    queue_type item;
+
+    sem_wait(&mN);
+    sem_wait(&mS);
+
+    // get the next item from the queue
+    item = mBuffer.at(mItemsOut);
+    mItemsOut = (mItemsOut + 1) % mItemsK;
+
+    sem_post(&mS);
+    sem_post(&mE);
+
+    return item;
+}
