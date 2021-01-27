021a1514ab811e5a1061192658d2424352bea2a0
Gabriel Sotelo <gabrielsotelojusto@hotmail.com>
2021-01-26 20:56:48 -0500

    sending single custom USER_10 msgs

--- web/app.py
+++ web/app.py
@@ -1,10 +1,18 @@
 from flask import Flask, render_template, request
 import can
+import cantools
 
 app = Flask(__name__)
 
+# Set up bus
 bustype = 'socketcan'
 channel = 'vcan0'
+
+bus = can.interface.Bus(bustype=bustype, channel=channel)
+
+# Get data from CAN2
+db = cantools.database.load_file("can_data/CAN2.dbc")
+user10_msg = db.get_message_by_name("USER_10")
 
 @app.route("/")
 def index():
@@ -29,21 +37,23 @@ def success():
 
 @app.route("/success2")
 def success2():
-    USER_10_ID = 0x2C6
     switch = request.args.get("switch")
     delay = request.args.get("delay")
     
-    if switch == "on":
-        for i in range(1, 5):
-            value = request.args.get(f"myRange{i}")
-            print(value)
-        
-        #send_message(USER_10_ID, data)
+    left = request.args.get("myRange1")
+    right = request.args.get("myRange2")
+    pump = request.args.get("myRange3")
+    brake =request.args.get("myRange4")
+    
+    data = user10_msg.encode({'USER_fanLeftOverride':left, 'USER_fanRightOverride':right, 
+                              'USER_wpOverride':pump, 'USER_brakeLightOverride':brake})
+    
+    msg = can.Message(arbitration_id=user10_msg.frame_id, data=data)
+    bus.send(msg)
     
     return render_template("success.html")
 
 def send_message(ID, data):
-    bus = can.interface.Bus(bustype=bustype, channel=channel)
     msg = can.Message(arbitration_id=int(ID),
                       data= [int(x, 16) for x in data],
                       is_extended_id=True)
