0d42099db64895d25dd6bbf0cce71c578368bcc6
Gabriel Sotelo <gabrielsotelojusto@hotmail.com>
2021-02-23 20:57:37 -0500

    user_11 and user_12 implementation

--- web/app.py
+++ web/app.py
@@ -18,6 +18,8 @@ bus = can.interface.Bus(bustype=bustype, channel=channel)
 # Get data from CAN2
 db = cantools.database.load_file("can_data/CAN2.dbc")
 user10_msg = db.get_message_by_name("USER_10")
+user11_msg = db.get_message_by_name("USER_11")
+user12_msg = db.get_message_by_name("USER_12")
 
 @app.route("/")
 def index():
@@ -26,6 +28,14 @@ def index():
 @socketio.on('update')
 def update(message):
     user10_message(message['left'], message['right'], message['pump'], message['brake'])
+
+@socketio.on('led_color')
+def led_color(message):
+    user11_message(message['color'])
+    
+@socketio.on('display_message')
+def display_message(message):
+    user12_message(message['text'])
 
 @socketio.on('disconnect')
 def terminate():
@@ -68,14 +78,24 @@ def user10_message(left, right, pump, brake):
     pump = int(pump)
     brake = int(brake)
     
-    print('Left:', left)
-    print('Right:', right)
-    print('Water Pump:', pump)
-    print('Brake:', brake)
-    
     data = user10_msg.encode({'USER_fanLeftOverride':left, 'USER_fanRightOverride':right, 'USER_wpOverride':pump, 'USER_brakeLightOverride':brake})
     msg = can.Message(arbitration_id=user10_msg.frame_id, data=data)
     bus.send(msg)
+
+def user11_message(val):
+    val = int(val)
+    data = user11_msg.encode({'USER_driverSignal':val})
+    msg = can.Message(arbitration_id=user11_msg.frame_id, data=data)
+    bus.send(msg)
     
+def user12_message(display):
+    signals = ["USER_driverMessageChar0", "USER_driverMessageChar1", "USER_driverMessageChar2", "USER_driverMessageChar3",
+               "USER_driverMessageChar4", "USER_driverMessageChar5", "USER_driverMessageChar6", "USER_driverMessageChar7"]
+    encoded = {i: 0 if j == '0' else ord(j) for i, j in zip(signals, display.ljust(8,'0'))}
+    
+    data = user12_msg.encode(encoded)
+    msg = can.Message(arbitration_id=user12_msg.frame_id, data=data)
+    bus.send(msg)
+
 if __name__ == "__main__":
     socketio.run(app)
