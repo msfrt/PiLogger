6f9934e6612f2b5a9d8ffb8b407257861bb2c051
Dave Yonkers <yonkers4@msu.edu>
2021-12-17 15:36:13 -0500

    use cd instead

--- logger/build.sh
+++ logger/build.sh
@@ -8,9 +8,10 @@ sudo apt-get install cmake -y
 
 # build and install dbcppp
 mkdir src/external/dbcppp/build
-cmake -DCMAKE_BUILD_TYPE=Release -Dbuild_tests=OFF -Dbuild_examples=OFF -S src/external/dbcppp -B src/external/dbcppp/build
-make -j src/external/dbcppp/build
-sudo make install src/external/dbcppp/build
+cd src/external/dbcppp/build
+cmake -DCMAKE_BUILD_TYPE=Release -Dbuild_tests=OFF -Dbuild_examples=OFF ..
+make -j
+sudo make install
 sudo ldconfig
 
 
