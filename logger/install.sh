0c17b53bf7ae7fbe6801d7fc0f747ce21d5a1b3d
Dave Yonkers <yonkers4@msu.edu>
2021-12-17 21:33:45 -0500

    update install

--- logger/install.sh
+++ logger/install.sh
@@ -3,6 +3,9 @@
 # make a folder in the etc directory to store the configuration file
 sudo mkdir /etc/PiLogger
 
+# disable the logger if it exists
+sudo systemctl stop PiLogger
+
 # move the config file to the etc directory and move the binary file to the bin directory
 sudo cp src/build/logger.conf /etc/PiLogger/
 sudo cp src/build/logger /bin/
@@ -11,6 +14,7 @@ sudo cp src/build/logger /bin/
 cp PiLogger.service /etc/systemd/system/
 
 # enable the services to start up on boot
-systemctl enable PiLogger
+sudo systemctl enable PiLogger
+sudo systemctl start PiLogger
 
 
